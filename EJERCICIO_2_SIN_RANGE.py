3350fc59d388dcc9da5decc62572103c1726d5fe
Marina Iglesia Pérez <iglesiaperezmarina@gmail.com>
2022-10-27 19:59:33 +0200

    formateo de codigo

--- EJERCICIO_2_SIN_RANGE.py
+++ EJERCICIO_2_SIN_RANGE.py
@@ -1,14 +1,13 @@
 #Crea un programa que pida por teclado introducir una contraseña. La contraseña no podrá tener menos de 8 caracteres ni espacios en blanco. 
 #Si la contraseña es correcta, el programa imprime “Contraseña OK”. En caso contrario imprime “Contraseña errónea”
 
-
 contrasena=(input("Introduce una contraseña correcta: "))
 
 contador=0
 
 for letra in contrasena:
-    if letra==" ":
-        contador+=1  
+    if letra == " ":
+        contador += 1  
 
 if len (contrasena)<8 or contador>0:
     print("Contraseña errónea")
